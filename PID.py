60b4f05a67c931e6962ea1ae5596ef401067e756
zapatistas <30812664+zapatistas@users.noreply.github.com>
2019-12-01 13:04:04 +0100

    PID
    bias delete , introduced dictionary functions for calculate

--- PID.py
+++ PID.py
@@ -1,36 +1,57 @@
 # noinspection PyAttributeOutsideInit
 class PID:
-    def __init__(self, kc, ti, td, timeStep, setValue, integralRange, integralFlag, clearErrorFlag, bias):
+    def __init__(self, kc, ti, td, timeStep, setValue, integralRange, calculateFlag):
         self.kc = kc
-        self.ki = kc/ti
+        self.ki = kc / ti
         self.kd = kc * td
         self.dt = timeStep
         self.integralError = 0
         self.integralRange = integralRange
         self.setValue = setValue
         self.previousControlVal = 0
-        self.integralFlag = integralFlag
-        self.clearErrorFlag = clearErrorFlag
-        self.bias = bias
+        self.calculateDictionary = {"noFlush": self.calculate_no_clear, "signChange": self.calculate_error_sign,
+                                    "rangeExit": self.calculate_range_exit}
+        self.calculate = self.calculateDictionary[calculateFlag]
 
-    def calculate(self, controlValue):
+    def calculate_no_clear(self, controlValue):
 
+        error = self.setValue - controlValue
+        self.accumulate_error(error, controlValue)
+        controlValDiff = controlValue - self.previousControlVal
+        output = self.kc * error + self.ki * self.integralError * self.dt - self.kd * controlValDiff / self.dt
+        self.previousControlVal = controlValue
+
+        return output
+
+    def calculate_error_sign(self, controlValue):
         error = self.setValue - controlValue
 
         previousError = self.integralError
 
-        if self.integralFlag == 0:
-            self.integralError += error
-        elif controlValue < self.integralRange[1] or controlValue > self.integralRange[0]:
-            self.integralError += error
-
-        if self.clearErrorFlag == 0 and (previousError > 0 and self.integralError < 0) or (previousError < 0 and self.integralError > 0):
-            self.integralError = 0
-        elif self.clearErrorFlag == 1 and (controlValue > self.integralRange[1] or controlValue < self.integralRange[0]):
+        if (previousError > 0 and error < 0) or (previousError < 0 and error > 0):
             self.integralError = 0
 
+        self.accumulate_error(error, controlValue)
         controlValDiff = controlValue - self.previousControlVal
-        output = self.bias + self.kc * error + self.ki * self.integralError * self.dt - self.kd * controlValDiff / self.dt
+        output = self.kc * error + self.ki * self.integralError * self.dt - self.kd * controlValDiff / self.dt
         self.previousControlVal = controlValue
 
         return output
+
+    def calculate_range_exit(self, controlValue):
+        error = self.setValue - controlValue
+
+        if controlValue > self.integralRange[1] or controlValue < self.integralRange[0]:
+            self.integralError = 0
+
+        self.accumulate_error(error, controlValue)
+        controlValDiff = controlValue - self.previousControlVal
+        output = self.kc * error + self.ki * self.integralError * self.dt - self.kd * controlValDiff / self.dt
+        self.previousControlVal = controlValue
+
+        return output
+
+    def accumulate_error(self, error, controlValue):
+
+        if self.integralRange[0] < controlValue < self.integralRange[1]:
+            self.integralError += error
