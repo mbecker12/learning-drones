aced1ec7f712151849fb1b316e763d9b004a7ed0
zapatistas <30812664+zapatistas@users.noreply.github.com>
2019-12-10 15:24:43 +0100

    PID
    changed the integral range to a float, in regards with the error

--- PID.py
+++ PID.py
@@ -1,7 +1,15 @@
 # noinspection PyAttributeOutsideInit
+import numpy as np
+
 class PID:
-    def __init__(self, kp, ki, kd, timeStep, setValue, integralRange, calculateFlag):
-        """ PID class, where ki = kp/ti and kd = kp*td set as input
+    def __init__(self, kp: float, ki: float, kd: float, timeStep: float, setValue, integralRange: float, calculateFlag):
+        """ PID class, where ki = kp/ti and kd = kp*td  constants set  from the parameters.py file
+            setValue  is the value we want the PID to reach
+
+            calculateFlag is  string that corresponds to a differentiation of the calculate function with the help
+            of a dictionary defined in the constructor as calculateDictionary
+
+            calculate is the method we use to find the output of the PID
         """
         self.kp = kp
         self.ki = ki
@@ -17,6 +25,11 @@ class PID:
         self.calculate = self.calculateDictionary[calculateFlag]
 
     def calculate_no_clear(self, controlValue):
+        """
+        This is a method to calculate output without flushing of error.
+        The derivative part at the first time of calculate call is 0.
+        :param controlValue: PV
+        """
 
         error = self.setValue - controlValue
         self.accumulate_error(error, controlValue)
@@ -32,6 +45,10 @@ class PID:
         return output
 
     def calculate_error_sign(self, controlValue):
+        """
+        Calculate but flush error when the previous error and the current error have different signs.
+        (When error is 0 it is not stored as previous error)
+        """
         error = self.setValue - controlValue
 
         errorSign = self.previousError * error
@@ -55,9 +72,12 @@ class PID:
         return output
 
     def calculate_range_exit(self, controlValue):
+        """
+        Same calculate, but flush error when error is greater than the integral range.
+        """
         error = self.setValue - controlValue
 
-        if controlValue > self.integralRange[1] or controlValue < self.integralRange[0]:
+        if np.abs(error) > self.integralRange:
             self.integralError = 0
         else:
             self.accumulate_error(error, controlValue)
@@ -72,7 +92,7 @@ class PID:
 
         return output
 
-    def accumulate_error(self, error, controlValue):
+    def accumulate_error(self, error):
 
-        if self.integralRange[0] < controlValue < self.integralRange[1]:
+        if np.abs(error) < self.integralRange:
             self.integralError += error
